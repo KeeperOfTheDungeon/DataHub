be55128913e42db18e8cf4b897d01677a96cba8a
Lukas Cebatavicius <lukasceb@outlook.com>
2025-03-19 14:45:25 +0100

    start

--- start.py
+++ start.py
@@ -1,13 +1,10 @@
 from Config.AntConfig import AntDeviceConfig
-from HeadSensorController import HeadSensorController
-from MotionController import MotionController
-
-device_type = AntDeviceConfig.DATA_HUB
+from RoboControl.Robot.Device.Generic.DataHub.DataHub import DataHub
 
 def main():
     print("Start main()")
 
-    device = DataHub(AntDeviceConfig.DATA_HUB)
+    device = DataHub(AntDeviceConfig.MAIN_DATA_HUB)
     device.run()
 
 
